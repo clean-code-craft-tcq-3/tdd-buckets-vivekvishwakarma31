f187fa6ddcd51fd652baca8a2dbe53da9c6434a0
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-06-09 19:08:39 +0530

    Update testcode.cpp

--- testcode.cpp
+++ testcode.cpp
@@ -5,7 +5,7 @@
 #include "chargingrange.h"
 
 //TEST CASE #1: Write only the test code without the actual definition in souce file
-TEST_CASE("TEST NO Definition") {
+TEST_CASE("TEST Function Not Declared") {
    vector <int> chargingSessionSamples = {3, 3, 5, 4, 10, 11, 12};   
    REQUIRE(getNumberofReadingsInRange(chargingSessionSamples, 3, 5) == 4);
 }
