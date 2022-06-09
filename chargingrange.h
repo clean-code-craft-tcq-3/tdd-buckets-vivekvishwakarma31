125b677adc608bcd82c5a3a6df19bb68b63a915f
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-06-09 19:09:16 +0530

    Update chargingrange.h

--- chargingrange.h
+++ chargingrange.h
@@ -1,1 +1,7 @@
-
+#include <iostream>
+#include<string>
+#include <vector>
+using namespace std;
+int detectRangeCount(vector<int> chargingSessionSamples, int lowerBound, int upperBound);
+int getNumberofReadingsInRange(vector<int> chargingSessionSamples, int lowerBound, int upperBound);
+void printInConsole(int lowerBound, int upperBound,int readingCount)
