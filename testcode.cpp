316b35d6f3824e96704a433839b1637690836a1d
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-06-09 19:03:55 +0530

    Update testcode.cpp

--- testcode.cpp
+++ testcode.cpp
@@ -1,6 +1,11 @@
-#include <stdio.h>
-int main()
-{
-  printf("Write test");
-  return 0;
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
+
+#include "chargingrange.h"
+
+//TEST CASE #1: Write only the test code without the actual definition in souce file
+TEST_CASE("TEST NO Definition") {
+   vector <int> chargingSessionSamples = {3, 3, 5, 4, 10, 11, 12};   
+   REQUIRE(getNumberofReadingsInRange(chargingSessionSamples, 3, 5) == 4);
 }
