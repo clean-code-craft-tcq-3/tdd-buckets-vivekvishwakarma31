88a3abc42960bd3b26eb6ee0b046f1b08094f73e
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-06-09 19:09:53 +0530

    Update testcode.cpp

--- testcode.cpp
+++ testcode.cpp
@@ -9,3 +9,9 @@ TEST_CASE("TEST Function Not Declared") {
    vector <int> chargingSessionSamples = {3, 3, 5, 4, 10, 11, 12};   
    REQUIRE(getNumberofReadingsInRange(chargingSessionSamples, 3, 5) == 4);
 }
+
+//TEST CASE #2: Declare Function in source file, so test will fail because definition is not present
+TEST_CASE("TEST NO Definition Failed Result") {
+   vector <int> chargingSessionSamples = {3, 3, 5, 4, 10, 11, 12};   
+   REQUIRE(getNumberofReadingsInRange(chargingSessionSamples, 3, 5) == 4);
+}
