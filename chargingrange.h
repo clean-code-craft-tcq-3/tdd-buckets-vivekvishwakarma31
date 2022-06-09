52957c94acf99a2eef828a64927f35b1de74b530
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-06-09 18:31:48 +0530

    Create chargingrange.h

---
+++ chargingrange.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include<string>
+#include <vector>
+using namespace std;
+int DetectRangeCount(vector<int> chargingSessionSamples, int lowerBound, int upperBound);
+int getNumberofReadingsInRange(vector<int> chargingSessionSamples, int lowerBound, int upperBound);
