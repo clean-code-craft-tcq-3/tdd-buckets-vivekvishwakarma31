ff61680e5da54e91c789c2dba9d6315163fb93e8
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-06-09 19:12:45 +0530

    Update testcode.cpp

--- testcode.cpp
+++ testcode.cpp
@@ -5,13 +5,15 @@
 #include "chargingrange.h"
 
 //TEST CASE #1: Write only the test code without the actual definition in souce file
-TEST_CASE("TEST Function Not Declared") {
+TEST_CASE("TEST Function Not Declared") 
+{
    vector <int> chargingSessionSamples = {3, 3, 5, 4, 10, 11, 12};   
    REQUIRE(getNumberofReadingsInRange(chargingSessionSamples, 3, 5) == 4);
 }
 
 //TEST CASE #2: Declare Function in source file, so test will fail because definition is not present
-TEST_CASE("TEST NO Definition Failed Result") {
+TEST_CASE("TEST NO Definition Failed Result")
+{
    vector <int> chargingSessionSamples = {3, 3, 5, 4, 10, 11, 12};   
    REQUIRE(getNumberofReadingsInRange(chargingSessionSamples, 3, 5) == 4);
 }
