77aaf558c9299f0aa80c05c390ee023385247ebb
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-06-29 19:05:20 +0530

    Update testcode.cpp

--- testcode.cpp
+++ testcode.cpp
@@ -6,5 +6,12 @@ int main()
    TestTDDStepFunctionNotDeclared();
    TestTDDStepNoFunctionDefinition();
    TestCurrentRange();
+   TestReadingForEmptySamples();
+   TestReadingForSingleSample();
+   TestReadingForOnlyLowerandUpperSamples();
+   TestReadingforOutofRangeSamples();
+   Test12BitConversion();
+   Test10BitConversion();
+   TestIgnoreErrorReadings();
 
 }
