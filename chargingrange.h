5fac5aa978e3a70cff590d3a60b095cdf5c8816b
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-06-29 10:19:16 +0530

    Update chargingrange.h

--- chargingrange.h
+++ chargingrange.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include<string>
 #include <vector>
+#include <assert.h>
 using namespace std;
 int detectRangeCount(vector<int> chargingSessionSamples, int lowerBound, int upperBound);
 int getNumberofReadingsInRange(vector<int> chargingSessionSamples, int lowerBound, int upperBound);
