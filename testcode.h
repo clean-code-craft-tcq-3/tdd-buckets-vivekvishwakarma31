ff575dfdc2968bc82dc370c990b4c4f78ff9a0d0
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-06-29 12:52:29 +0530

    Update testcode.h

--- testcode.h
+++ testcode.h
@@ -25,16 +25,36 @@ void TestCurrentRange()
 
    //Test 3-5 Current Range Where 3- Lower Bound, 5 - Upper Bound
     assert(getNumberofReadingsInRange(chargingSessionSamples_1, 3, 5) == 4);
-    assert(getNumberofReadingsInRange(chargingSessionSamples_2, 3, 5) == 2);
-    assert(getNumberofReadingsInRange(chargingSessionSamples_3, 3, 5) == 1);
-    assert(getNumberofReadingsInRange(chargingSessionSamples_4, 3, 5) == 0);
-
    
     //Test 10-12 Current Range Where 10- Lower Bound, 12 - Upper Bound
     assert(getNumberofReadingsInRange(chargingSessionSamples_1, 10, 12) == 3);
-    assert(getNumberofReadingsInRange(chargingSessionSamples_2, 10, 12) == 2);
-    assert(getNumberofReadingsInRange(chargingSessionSamples_3, 10, 12) == 1);
-    assert(getNumberofReadingsInRange(chargingSessionSamples_4, 10, 12) == 0);
+
 
 }
+void TestReadingForEmptySamples()
+{
+   vector <int> chargingSessionSamples = {};
+   assert(getNumberofReadingsInRange(chargingSessionSamples_4, 3, 5) == 0);
+   assert(getNumberofReadingsInRange(chargingSessionSamples_4, 10, 12) == 0);
+}
+void TestReadingForSingleSample()
+{
+   vector <int> chargingSessionSamples = {5};
+   vector <int> chargingSessionSamples_2 = {12};
+   assert(getNumberofReadingsInRange(chargingSessionSamples, 3, 5) == 1);
+   assert(getNumberofReadingsInRange(chargingSessionSamples_2, 10, 12) == 1);
+}
+void TestReadingForOnlyLowerandUpperSamples()
+{
+    vector <int> chargingSessionSamples = {3,5};
+    vector <int> chargingSessionSamples_2 = {10,12};
+   assert(getNumberofReadingsInRange(chargingSessionSamples, 3, 5) == 2);
+   assert(getNumberofReadingsInRange(chargingSessionSamples_2, 10, 12) == 2);
+}
 
+void TestReadingforOutofRangeSamples()
+{
+    vector <int> chargingSessionSamples = {1,15};
+   assert(getNumberofReadingsInRange(chargingSessionSamples, 3, 5) == 0);
+   assert(getNumberofReadingsInRange(chargingSessionSamples, 10, 12) == 0);
+}
