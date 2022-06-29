dc343a159bc7de800ca26a1121a3996e658bc402
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-06-29 10:55:34 +0530

    Update chargingrange.cpp

--- chargingrange.cpp
+++ chargingrange.cpp
@@ -18,10 +18,13 @@ void printInConsole(int lowerBound, int upperBound,int readingCount)
   cout << " Range -"<< " lowerBound : "<<lowerBound << " upperBound : " <<upperBound << " Reading Count : "<< readingCount << "\n";
 }
 
-int detectRangeCount(vector<int> chargingSessionSamples, int lowerBound, int upperBound) {
+int detectRangeCount(vector<int> chargingSessionSamples, int lowerBound, int upperBound)
+{
     int countOfReadingsFromRange = 0;
-    for(int i = 0; i < chargingSessionSamples.size(); i++) {
-      if((chargingSessionSamples[i] >= lowerBound) && (chargingSessionSamples[i] <= upperBound)) {
+    for(int i = 0; i < chargingSessionSamples.size(); i++) 
+    {
+      if((chargingSessionSamples[i] >= lowerBound) && (chargingSessionSamples[i] <= upperBound))
+      {
         countOfReadingsFromRange++;
       }
     } 
