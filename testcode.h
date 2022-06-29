965df7483adbb0f2c4b0b1a6dabf0d27a95671f6
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-06-29 10:23:13 +0530

    Update testcode.h

--- testcode.h
+++ testcode.h
@@ -1,4 +1,19 @@
 #ifndef TESTCODE_H
 #define TESTCODE_H
 #include "chargingrange.h"
+
+
+//TEST CASE #1: Write only the test code without the actual definition in souce file
+void TestTDDStepFunctionNotDeclared()
+{
+   vector <int> chargingSessionSamples = {3, 3, 5, 4, 10, 11, 12};   
+   assert(getNumberofReadingsInRange(chargingSessionSamples, 3, 5) == 4);
+}
+
+//TEST CASE #2: Declare Function in source file, so test will fail because definition is not present
+void TestTDDStepNoFunctionDefinition()
+{
+   vector <int> chargingSessionSamples = {3, 3, 5, 4, 10, 11, 12};   
+   REQUIRE(getNumberofReadingsInRange(chargingSessionSamples, 3, 5) == 4);
+}
 #endif
