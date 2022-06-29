accbe1b0d7191d14b5138d3f8f581c87f377c0ce
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-06-29 11:49:22 +0530

    Update testcode.h

--- testcode.h
+++ testcode.h
@@ -15,3 +15,26 @@ void TestTDDStepNoFunctionDefinition()
    assert(getNumberofReadingsInRange(chargingSessionSamples, 3, 5) == 4);
 }
 
+//TEST CASE #3 : Test Current Range 
+void TestCurrentRange()
+{
+   vector <int> chargingSessionSamples_1 = {3, 3, 5, 4, 10, 11, 12};
+   vector <int> chargingSessionSamples_2 = {5, 4, 10, 11};
+   vector <int> chargingSessionSamples_3 = {3,10};
+   vector <int> chargingSessionSamples_4;
+
+   //Test 3-5 Current Range Where 3- Lower Bound, 5 - Upper Bound
+    assert(getNumberofReadingsInRange(chargingSessionSamples_1, 3, 5) == 4);
+    assert(getNumberofReadingsInRange(chargingSessionSamples_2, 3, 5) == 2);
+    assert(getNumberofReadingsInRange(chargingSessionSamples_3, 3, 5) == 1);
+    assert(getNumberofReadingsInRange(chargingSessionSamples_4, 3, 5) == 0);
+
+   
+    //Test 10-12 Current Range Where 10- Lower Bound, 12 - Upper Bound
+    assert(getNumberofReadingsInRange(chargingSessionSamples_1, 10, 12) == 3);
+    assert(getNumberofReadingsInRange(chargingSessionSamples_2, 10, 12) == 2);
+    assert(getNumberofReadingsInRange(chargingSessionSamples_3, 10, 12) == 1);
+    assert(getNumberofReadingsInRange(chargingSessionSamples_4, 10, 12) == 0);
+
+}
+
